b4ff706ee0e38d7adfd8bbf17f11d60ac78eae8d
joanaraposo96 <joanaraposo96@outlook.pt>
2026-04-15 15:50:30 +0200

    Fixed code

--- cypress/e2e/apitest.cy.js
+++ cypress/e2e/apitest.cy.js
@@ -8,6 +8,8 @@ describe('Testes da API de Carros', () => {
     preco: 80000
   }
 
+  let storedCar;
+
   it('Listar todos os carros', () => {
     cy.request({
       method: 'GET',
@@ -42,28 +44,44 @@ describe('Testes da API de Carros', () => {
         method: 'POST',
         url: '/carros',
         body: newCar
-      }).then((response) => {
+      }).then((response) => { 
         allCars.push(response.body);
         expect(response.status).to.equal(201);
         expect(response.body.id).to.exist;
         cy.log(JSON.stringify(response.body));
+        if (i == 1) {
+          storedCar = response.body;
+        }
       });
     }
   });
 
-    it('Deletar um novo carro', () => {
-      const carsToDelete = allCars.slice(2)
-      cy.wrap(carsToDelete).each((element) => {
-        cy.request({
-          method: 'DELETE',
-          url: `/carros/${element.id}`
-        }).then((response) => {
-          expect(response.status).to.equal(200);
-          cy.log(JSON.stringify({
-            id: element.id,
-            mensagem: response.body.mensagem
-          }));
-        });
-      })
-    });
+  it('Deletar um carro específico', () => {
+    cy.request({
+        method: 'DELETE',
+        url: `/carros/${storedCar.id}`
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+        cy.log(JSON.stringify({
+          id: storedCar.id,
+          mensagem: response.body.mensagem
+        }));
+      });
   });
+    
+  it.skip('Deletar todos os novos carros', () => {
+    const carsToDelete = allCars.slice(2)
+    cy.wrap(carsToDelete).each((element) => {
+      cy.request({
+        method: 'DELETE',
+        url: `/carros/${element.id}`
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+        cy.log(JSON.stringify({
+          id: element.id,
+          mensagem: response.body.mensagem
+        }));
+      });
+    })
+  });
+});
